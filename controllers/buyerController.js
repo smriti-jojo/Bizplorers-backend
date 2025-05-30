bd8011da12ef83f5b3e0058a6962995b7e5f3c1a
smriti-figurecircle <smriti30jojo@gmail.com>
2025-05-30 18:53:47 +0530

    error resolved

--- controllers/buyerController.js
+++ controllers/buyerController.js
@@ -33,16 +33,39 @@ exports.fillBuyerDetails = async (req, res) => {
 //   }
 // };
 
-
-// exports.getBuyerDetails = async (req, res) => {
+// exports.fillBuyerDetails = async (req, res) => {
 //   try {
-//     const buyer = await Buyer.findOne({ where: { userId: req.user.id } });
-//     if (!buyer) return res.status(404).json({ error: 'No buyer details found' });
-//     res.status(200).json(buyer);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
+//     const existingSeller = await Seller.findOne({ where: { userId: req.user.id } });
+//     const existingBuyer = await Buyer.findOne({ where: { userId: req.user.id } });
+//     const existingBroker = await Broker.findOne({ where: { userId: req.user.id } });
+
+//     if (existingSeller || existingBuyer || existingBroker) {
+//       return res.status(400).json({ message: "You already have a profile as Buyer, Seller, or Broker." });
+//     }
+
+//     let payload = { ...req.body, userId: req.user.id };
+
+    
+//     if (req.user.role === 'broker' && req.body.brokerId) {
+//       payload.brokerId = req.body.brokerId;
+//     }
+
+//     const buyer = await Buyer.create(payload);
+//     res.status(201).json(buyer);
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
 //   }
 // };
+
+exports.getBuyerDetails = async (req, res) => {
+  try {
+    const buyer = await Buyer.findOne({ where: { userId: req.user.id } });
+    if (!buyer) return res.status(404).json({ error: 'No buyer details found' });
+    res.status(200).json(buyer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.updateBuyerDetails = async (req, res) => {
   try {
