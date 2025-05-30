ba1a18d5a5e36ab0d82961f74d78e8f1b6dd8d62
smriti-figurecircle <smriti30jojo@gmail.com>
2025-05-30 23:39:37 +0530

    id sent during login

--- controllers/authController.js
+++ controllers/authController.js
@@ -147,7 +147,7 @@ exports.login = async (req, res) => {
     if (!valid) return res.status(401).json({ error: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
-    return res.json({ token ,user:{email:email,role:user.role}});
+    return res.json({ token ,user:{email:email,role:user.role,id:user.id}});
   } catch (err) {
     console.error('Login error:', err);
     return res.status(500).json({ error: 'Login failed due to server error.' });
