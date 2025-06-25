499eeb59085307c9f189ae9572bff0ba85fab6a3
smriti-figurecircle <smriti30jojo@gmail.com>
2025-06-25 14:01:07 +0530

    added user details

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,40 @@
 const {User}=require('../models');
 
+// exports.getAllUsers = async (req, res) => {
+//   try {
+//     const users = await User.findAll(); // You can also add attributes if needed
+//     res.status(200).json({ success: true, data: users });
+//   } catch (error) {
+//     console.error('Error fetching users:', error);
+//     res.status(500).json({ success: false, message: 'Server error' });
+//   }
+// };
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.findAll(); // You can also add attributes if needed
+    const users = await User.findAll({
+      include: [
+        {
+          model: Seller,
+          as: 'seller',
+        },
+        {
+          model: Buyer,
+          as: 'buyer',
+        },
+        {
+          model: Broker,
+          as: 'broker',
+        },
+      ],
+    });
+
     res.status(200).json({ success: true, data: users });
   } catch (error) {
     console.error('Error fetching users:', error);
     res.status(500).json({ success: false, message: 'Server error' });
   }
 };
+
 
 exports.updateUser = async (req, res) => {
   try {
