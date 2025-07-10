31a652179187e7ba4fe270190184d820d3a790be
smriti-figurecircle <smriti30jojo@gmail.com>
2025-07-10 23:04:43 +0530

    picklist controller unique id added

--- models/pickListValue.js
+++ models/pickListValue.js
@@ -30,6 +30,51 @@
 //   return PicklistValue;
 // };
 
+// module.exports = (sequelize, DataTypes) => {
+//   const PicklistValue = sequelize.define('PicklistValue', {
+//     value: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     is_active: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: true,
+//     },
+//     parent_id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+//   }, {
+//     timestamps: true,
+//   });
+
+//   PicklistValue.associate = (models) => {
+//     PicklistValue.belongsTo(models.PicklistCategory, {
+//       foreignKey: 'category_id',
+//       onDelete: 'CASCADE',
+//     });
+
+//     models.PicklistCategory.hasMany(PicklistValue, {
+//       foreignKey: 'category_id',
+//       onDelete: 'CASCADE',
+//     });
+
+//     // ✅ Self-referencing association
+//     PicklistValue.belongsTo(models.PicklistValue, {
+//       as: 'parent',
+//       foreignKey: 'parent_id',
+//     });
+
+//     PicklistValue.hasMany(models.PicklistValue, {
+//       as: 'children',
+//       foreignKey: 'parent_id',
+//     });
+//   };
+
+//   return PicklistValue;
+// };
+
+
 module.exports = (sequelize, DataTypes) => {
   const PicklistValue = sequelize.define('PicklistValue', {
     value: {
@@ -43,12 +88,19 @@ module.exports = (sequelize, DataTypes) => {
     parent_id: {
       type: DataTypes.INTEGER,
       allowNull: true,
-    },
+    }
   }, {
     timestamps: true,
+    indexes: [
+      {
+        unique: true,
+        fields: ['value', 'category_id', 'parent_id']
+      }
+    ]
   });
 
   PicklistValue.associate = (models) => {
+    // Category relation
     PicklistValue.belongsTo(models.PicklistCategory, {
       foreignKey: 'category_id',
       onDelete: 'CASCADE',
@@ -59,7 +111,7 @@ module.exports = (sequelize, DataTypes) => {
       onDelete: 'CASCADE',
     });
 
-    // ✅ Self-referencing association
+    // Self-referencing relation
     PicklistValue.belongsTo(models.PicklistValue, {
       as: 'parent',
       foreignKey: 'parent_id',
